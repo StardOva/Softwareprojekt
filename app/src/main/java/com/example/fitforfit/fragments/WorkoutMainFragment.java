78f1743c707343573b06058a7d4e19b462a85896
Moritz Reichelt <41913675+StardOva@users.noreply.github.com>
2022-01-25 10:31:23 +0100

    create workout feature

--- app/src/main/java/com/example/fitforfit/fragments/WorkoutMainFragment.java
+++ app/src/main/java/com/example/fitforfit/fragments/WorkoutMainFragment.java
@@ -1,6 +1,5 @@
 package com.example.fitforfit.fragments;
 
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -8,23 +7,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.fitforfit.R;
-import com.example.fitforfit.dao.WorkoutDao;
+import com.example.fitforfit.adapter.WorkoutListAdapter;
 import com.example.fitforfit.database.AppDatabase;
 import com.example.fitforfit.databinding.FragmentMainBinding;
 import com.example.fitforfit.entity.Workout;
 import com.example.fitforfit.singleton.Database;
 import com.example.fitforfit.ui.main.CreateNewWorkoutActivity;
 
+import java.util.List;
+
 public class WorkoutMainFragment extends Fragment {
 
     private FragmentMainBinding binding;
+    private WorkoutListAdapter workoutListAdapter;
 
     public WorkoutMainFragment(){
         super(R.layout.fragment_workout);
@@ -43,21 +46,36 @@ public class WorkoutMainFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         Button createNewWorkout = view.findViewById(R.id.btnNewWorkout);
         createNewWorkout.setOnClickListener(view1 -> {
-            /*
-            AsyncTask.execute(() -> {
-                AppDatabase db = Database.getInstance(getContext());
-
-                String workoutName = "Das Workout";
-
-                Workout workout = new Workout();
-                workout.name = workoutName;
-                db.workoutDao().insert(workout);
-            });
-            */
-
             Intent intent = new Intent(view.getContext(), CreateNewWorkoutActivity.class);
             view.getContext().startActivity(intent);
-
         });
+
+        initRecyclerView(view);
+        loadWorkoutList();
+    }
+
+    private void initRecyclerView(View view){
+        AsyncTask.execute(() -> {
+            RecyclerView recyclerView = view.findViewById(R.id.recyclerViewWorkouts);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+            this.workoutListAdapter = new WorkoutListAdapter(getActivity());
+            recyclerView.setAdapter(workoutListAdapter);
+        });
+    }
+
+    private void loadWorkoutList(){
+        AppDatabase db = Database.getInstance(getContext());
+        List<Workout> workoutList = db.workoutDao().getAll();
+        this.workoutListAdapter.setContext(getContext());
+        this.workoutListAdapter.setWorkoutList(workoutList);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // nach Anlegen eines neuen Workouts in der CreateNewWorkoutActivity
+        // muss die Liste neu geladen werden, damit das neue Workout auch dargestellt wird
+        loadWorkoutList();
     }
 }
