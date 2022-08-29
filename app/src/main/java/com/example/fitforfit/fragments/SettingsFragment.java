01b0cf7e84a52e8f17c9253576710d424acf5e9a
Griestopff <christophmanitz@gmail.com>
2022-08-29 11:24:03 +0200

    upload/download funktion + write Data to file

--- app/src/main/java/com/example/fitforfit/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/fitforfit/fragments/SettingsFragment.java
@@ -1,13 +1,16 @@
 package com.example.fitforfit.fragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.InputType;
 
 import androidx.annotation.Nullable;
 import androidx.preference.EditTextPreference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 
 import com.example.fitforfit.R;
+import com.example.fitforfit.sync.DatabaseSync;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
@@ -26,8 +29,24 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
         if (backup != null) {
             backup.setOnBindEditTextListener(editText -> {
-                editText.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+                editText.setInputType(InputType.TYPE_TEXT_FLAG_AUTO_COMPLETE | InputType.TYPE_CLASS_TEXT);
             });
+
+            SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this.getContext());
+            String urlString = sharedPrefs.getString("backup", "localhost");
+            DatabaseSync.uploadDB(this.getContext(), urlString);
+        }
+
+        EditTextPreference backupDown = findPreference("backup_down");
+
+        if (backup != null) {
+            backupDown.setOnBindEditTextListener(editText -> {
+                editText.setInputType(InputType.TYPE_TEXT_FLAG_AUTO_COMPLETE | InputType.TYPE_CLASS_TEXT);
+            });
+
+            SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this.getContext());
+            String urlString = sharedPrefs.getString("backup_down", "localhost");
+            DatabaseSync.downloadDB(this.getContext(), urlString);
         }
     }
 }
--- app/src/main/java/com/example/fitforfit/sync/DatabaseSync.java
+++ app/src/main/java/com/example/fitforfit/sync/DatabaseSync.java
@@ -1,25 +1,122 @@
 package com.example.fitforfit.sync;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
 
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.io.File;
 
-public class DatabaseSync {
+public class DatabaseSync extends AppCompatActivity {
+    // die Zeit aus den Shared Preferences auslesen
+    // SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+    // String urlString = sharedPrefs.getString("backup", "localhost");
+    //String urlString = "localhost";
 
-    private static void uploadDB(Context context) {
+    static String dbName = "com.example.fitforfit.database.AppDatabase";
+    static String filePath = "/path/to/file";
+
+
+
+
+    public static void uploadDB(Context context, String urlString) {
+        Log.d("UPLOAD", "Datei hochladen...");
+        File dbFile = context.getDatabasePath(dbName);
+        String path = dbFile.getAbsolutePath();
+
+        OutputStream out = null;
+
+        // file to byte[], Path
+        try {
+            byte[] bytesData = Files.readAllBytes(Paths.get(path));
+            try {
+                URL url = new URL(urlString);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("POST");
+                out = new BufferedOutputStream(urlConnection.getOutputStream());
+
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, "UTF-8"));
+                writer.write(String.valueOf(bytesData));
+                writer.flush();
+                writer.close();
+                out.close();
+
+                urlConnection.connect();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static void downloadDB(Context context, String urlString) {
+
+        File dbFile = context.getDatabasePath(dbName);
+        String path = dbFile.getAbsolutePath();
+        String data = null;
+
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader((new InputStreamReader(inputStream)));
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                data = data + line;
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            //codeText.setText("SEITE NICHT ERREICHBAR");
+            //error = "SEITE NICHT ERREICHBAR";
+            //startErrorActivity(error);
+            //Intent mir error String an ErrorScanActivity
+        } catch (IOException e) {
+            e.printStackTrace();
+            //codeText.setText("KEINE NETZWERKVERBINDUNG");
+            //error = "KEINE NETZWERKVERBINDUNG";
+            //startErrorActivity(error);
+        }
+        writeToFile(data, context);
+    }
+
+
+
+    private static void writeToFile(String data, Context context) {
+
+        File dbFile = context.getDatabasePath(dbName);
+        String path = dbFile.getAbsolutePath();
+
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput(path, Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+    /*
+    private static void downloadDB(Context context) {
         String urlString = "localhost";
         String dbName = "com.example.fitforfit.database.AppDatabase";
         String filePath = "/path/to/file";
@@ -53,7 +150,7 @@ public class DatabaseSync {
         }
 
 
-    }
+    }*/
 
 }
 
